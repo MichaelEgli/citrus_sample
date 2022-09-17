608369932d88a35fb35d92cf58da7bd9a34bdaa4
MichaelEgli <michaelegli1879@gmail.com>
2022-09-17 13:40:06 +0200

    update testng working

--- readme.md
+++
@@ -1,2 +0,0 @@
-# citrus_sample
-https://www.youtube.com/watch?v=2aMHqK6VgIg&list=PLCStwJYVkwTZGC-29trxNkAZIvveMOlca&index=2
---
+++ src/test/java/com/console/citrus/sample/EglisFirstCitrusTest.java
@@ -0,0 +1,27 @@
+package com.console.citrus.sample;
+
+import static com.consol.citrus.actions.EchoAction.Builder.echo;
+
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.testng.TestNGCitrusSupport;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+/**
+ * Eglis erster Citrus Test
+ *
+ * @author Michael Egl
+ * @since 2022-09-16
+ */
+public class EglisFirstCitrusTest extends TestNGCitrusSupport {
+    @CitrusTest
+    @Test
+    @Parameters("runner")
+    public void eglisFirstCitrusTest(@CitrusResource @Optional TestCaseRunner runner) {
+        runner.run(echo("TODO: Code the test EglisFirstCitrusTest"));
+
+    }
+}
