4552cbb06ef2731344f257e68508da7c094fd1d9
MichaelEgli <michaelegli1879@gmail.com>
2022-10-04 14:35:29 +0200

    update DesignerIT after log fix

--- src/test/java/com/consol/citrus/SampleJavaDesignerDrawbackIT.java
+++ src/test/java/com/consol/citrus/SampleJavaDesignerDrawbackIT.java
@@ -2,27 +2,29 @@ package com.consol.citrus;
 
 import com.consol.citrus.actions.AbstractTestAction;
 import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
 import org.testng.annotations.Test;
-import com.consol.citrus.testng.TestNGCitrusSupport;
+
 import utils.LoggingService;
 
 import static com.consol.citrus.DefaultTestActionBuilder.action;
+import static com.consol.citrus.actions.EchoAction.Builder.echo;
 
 @Test
-public class SampleJavaDesignerDrawbackIT extends TestNGCitrusSupport {
+public class SampleJavaDesignerDrawbackIT extends TestNGCitrusSpringSupport {
 
     LoggingService obj = new LoggingService();
     @CitrusTest
     public void testLogService()
     {
-        System.out.println("Before log called");
-        obj.logs("huhu");
-        action(new AbstractTestAction() {
+        $(echo("Before log called"));
+        obj.logs("Logs will be printed");
+/*        action(new AbstractTestAction() {
             @Override
             public void doExecute(com.consol.citrus.context.TestContext testContext) {
                 obj.logs("Logs will be printed....");
             }
-        });
-        System.out.println("After log called");
+        });*/
+        $(echo("After log called"));
     }
 }
